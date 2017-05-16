b0a15fbc82a4f1071d9afb8fe68830e9227e8cdd
Trapenok Victor <levhav@yandex.ru>
2017-05-16 11:50:41 +1000

    Add reactive to JUST.JS

--- examples/example.js
+++
@@ -1,8 +0,0 @@
-var JUST = require('./../index');
-
-var just = new JUST({ root : __dirname + '/view', useCache : true, ext : '.html' });
-
-just.render('page', { title: 'Hello, World!' }, function(error, html) {
-	console.log(error);
-	console.log(html);
-});
--- examples/view/header.html
+++
@@ -1,1 +0,0 @@
-<h1>Page title</h1>
